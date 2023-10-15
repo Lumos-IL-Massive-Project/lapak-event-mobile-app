35ac67b689db9a0e3bcf3aacd786434f53ed9a00
Antoni Luthfi <luthfiantoni027@gmail.com>
2023-10-16 01:03:20 +0800

    create notification list

---
+++ app/src/main/java/com/example/connectus/activities/notificationlist/NotificationListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.connectus.activities.notificationlist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.connectus.R
+
+class NotificationListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notification_list)
+    }
+}
---
+++ app/src/main/java/com/example/connectus/activities/notificationlist/model/NotificationData.kt
@@ -0,0 +1,8 @@
+package com.example.connectus.activities.chatlist.model
+
+data class ChatData(
+    val senderName: String,
+    val senderProfileImageUrl: String,
+    val message: String,
+    val createdAt: String
+)
