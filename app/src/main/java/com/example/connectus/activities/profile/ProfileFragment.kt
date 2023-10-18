e1ba129971735f76eb14a2ae04ca3089f306578e
Antoni Luthfi <luthfiantoni027@gmail.com>
2023-10-18 22:28:58 +0800

    create account page

--- app/src/main/java/com/example/connectus/activities/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/connectus/activities/profile/ProfileFragment.kt
@@ -5,19 +5,36 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.connectus.activities.signin.SignInActivity
 import com.example.connectus.databinding.FragmentProfileBinding
+import com.example.connectus.utils.startDynamicActivity
 
-class ProfileFragment : Fragment(), View.OnClickListener {
-    private lateinit var binding: FragmentProfileBinding
+class ProfileFragment : Fragment() {
+    private var binding: FragmentProfileBinding? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentProfileBinding.inflate(inflater, container, false)
-        return binding.root
+        initTopBar()
+        initLayout()
+        return binding!!.root
     }
 
-    override fun onClick(v: View?) {
-        TODO("Not yet implemented")
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+
+    private fun initTopBar() {
+        binding?.customTopBar?.ivBack?.visibility = View.GONE
+        binding?.customTopBar?.tvTopBarTitle?.text = "Profil"
+    }
+
+    private fun initLayout() {
+        binding?.signOutButton?.setOnClickListener {
+            startDynamicActivity(requireContext(), SignInActivity::class.java)
+        }
     }
 }
