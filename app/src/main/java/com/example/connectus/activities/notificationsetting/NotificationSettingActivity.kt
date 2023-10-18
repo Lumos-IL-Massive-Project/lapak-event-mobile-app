416f68c8cbe0bd4f121b646e9b6404023fd1b0cf
Antoni Luthfi <luthfiantoni027@gmail.com>
2023-10-19 01:38:24 +0800

    create notification setting page

---
+++ app/src/main/java/com/example/connectus/activities/notificationsetting/NotificationSettingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.connectus.activities.notificationsetting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.connectus.R
+
+class NotificationSettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notification_setting)
+    }
+}
