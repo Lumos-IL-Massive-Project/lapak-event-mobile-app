b35b6cfc0b77eb0359dcdfd0a79820402242c828
abdussalam21 <abdus7630@gmail.com>
2023-11-20 16:53:33 +0700

    add payment bank transfer

---
+++ app/src/main/java/com/example/connectus/activities/paymentbanktransfer/PaymentBankTransferActivity.kt
@@ -0,0 +1,12 @@
+package com.example.connectus.activities.paymentbanktransfer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.connectus.R
+
+class PaymentBankTransferActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment_bank_transfer)
+    }
+}
