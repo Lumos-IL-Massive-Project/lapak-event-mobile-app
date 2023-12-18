afdf19157ce90b6ad8c0fd3b05ce581456f200b9
antoniluthfi <luthfiantoni027@gmail.com>
2023-12-19 00:00:18 +0800

    do refresh token on splash screen

--- app/src/main/java/com/example/connectus/activities/splash/SplashActivity.kt
+++ app/src/main/java/com/example/connectus/activities/splash/SplashActivity.kt
@@ -2,17 +2,23 @@ package com.example.connectus.activities.splash
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import com.example.connectus.R
 import com.example.connectus.activities.MainActivity
 import com.example.connectus.activities.onboarding.OnboardingActivity
 import com.example.connectus.activities.signin.SignInActivity
+import com.example.connectus.activities.splash.viewmoodels.SplashViewModel
 import com.example.connectus.databinding.ActivitySplashBinding
+import com.example.connectus.network.ApiResult
+import com.example.connectus.network.bodyrequest.RefreshTokenBody
+import com.example.connectus.network.response.LoginData
 import com.example.connectus.utils.AppPreferenceManager
 import com.example.connectus.utils.startDynamicActivity
+import com.google.gson.Gson
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -22,6 +28,7 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySplashBinding
+    private val viewModel by viewModels<SplashViewModel>()
 
     @Inject
     lateinit var appPreferenceManager: AppPreferenceManager
@@ -32,41 +39,69 @@ class SplashActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         init()
+        initRefreshTokenObserver()
     }
 
     @OptIn(DelicateCoroutinesApi::class)
     private fun init() {
-        val onboardingStatus = appPreferenceManager.getOnboardingStatus()
-        val loginStatus = appPreferenceManager.getLoginStatus()
+        val authCredential = appPreferenceManager.getAuthCredentials()
 
-        GlobalScope.launch(Dispatchers.Main) {
-            delay(3000)
-            if (onboardingStatus) {
-                if (loginStatus) {
+        if (authCredential?.isNotEmpty() == true) {
+            val loginData = Gson().fromJson(authCredential, LoginData::class.java)
+            viewModel.refreshToken(loginData.id!!, RefreshTokenBody(loginData.refreshToken!!))
+        } else {
+            GlobalScope.launch {
+                delay(3000)
+                startDynamicActivity(
+                    this@SplashActivity,
+                    SignInActivity::class.java,
+                    R.anim.fade_in,
+                    R.anim.fade_out
+                )
+                finish()
+            }
+        }
+    }
+
+    private fun initRefreshTokenObserver() {
+        viewModel.refreshTokenResult.observe(this, Observer { result ->
+            when (result) {
+                is ApiResult.Error -> {
+                    appPreferenceManager.removeAuthCredentials()
+                    val onboardingStatus = appPreferenceManager.getOnboardingStatus()
+
+                    if (onboardingStatus) {
+                        startDynamicActivity(
+                            this@SplashActivity,
+                            SignInActivity::class.java,
+                            R.anim.fade_in,
+                            R.anim.fade_out
+                        )
+                    } else {
+                        startDynamicActivity(
+                            this@SplashActivity,
+                            OnboardingActivity::class.java,
+                            R.anim.fade_in,
+                            R.anim.fade_out
+                        )
+                    }
+                    finish()
+                }
+
+                is ApiResult.Loading -> {}
+                is ApiResult.Success -> {
+                    val userDataJson = Gson().toJson(result.data.data)
+                    appPreferenceManager.saveAuthCredentials(userDataJson)
+
                     startDynamicActivity(
                         this@SplashActivity,
                         MainActivity::class.java,
                         R.anim.fade_in,
                         R.anim.fade_out
                     )
-                } else {
-                    startDynamicActivity(
-                        this@SplashActivity,
-                        SignInActivity::class.java,
-                        R.anim.fade_in,
-                        R.anim.fade_out
-                    )
+                    finish()
                 }
-            } else {
-                startDynamicActivity(
-                    this@SplashActivity,
-                    OnboardingActivity::class.java,
-                    R.anim.fade_in,
-                    R.anim.fade_out
-                )
             }
-
-            finish()
-        }
+        })
     }
 }
